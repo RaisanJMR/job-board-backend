025aef7a4c88517646fddee62d8fe5faa7ce36c7
RaisanJMR <raisanjmr@protonmail.com>
2025-07-28 23:48:24 +0530

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# job-board-backend
+
+# Job Board Backend
+
+This is the backend for the **Job Board** application. It provides RESTful APIs to manage job listings.
+
+## Deployed URL
+
+**Backend**: [https://job-board-backend-vy6y.onrender.com/](https://job-board-backend-vy6y.onrender.com/)
+
+## Tech Stack
+
+- Node.js
+- Express.js
+- MongoDB
+- Mongoose
+- CORS
+- dotenv
+
+## Available API Endpoints
+
+
+- `GET /job` - Get all jobs with optional search by `title` and `location`
+- `GET /job/:id` - Get a single job by ID
+- `POST /job` - Create a new job 
+
+## Postman Collection 
+
+[https://documenter.getpostman.com/view/8886902/2sB3B7PEX4](https://documenter.getpostman.com/view/8886902/2sB3B7PEX4) 
--- index.js
+++ index.js
@@ -18,8 +18,15 @@ app.use('/api/job', jobRouter)
 
 
 app.get('/', (req, res) => {
-    res.send('api is running...')
-})
+    res.send(`
+      <h1>Welcome to the Job Board API</h1>
+      <ul>
+        <li><a href="https://job-board-backend-vy6y.onrender.com/api/job">GET /api/job</a> - List all jobs (supports ?title= & location=)</li>
+        <li><a href="https://job-board-backend-vy6y.onrender.com/api/job/your-job-id">GET /api/job/:id</a> - Get job by ID (replace <code>your-job-id</code>)</li>
+      </ul>
+      <p><a href="https://documenter.getpostman.com/view/8886902/2sB3B7PEX4" target="_blank">click here</a> to view api documentation in postman</p>
+    `);
+  });
 
 app.use(errorHandler)
 app.listen(PORT, () =>
